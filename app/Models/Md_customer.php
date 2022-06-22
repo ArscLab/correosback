6cdb2e66a31ed14a0733d596f6028ca6b3485241
Arsc <mauricio.perez.gomez2020@gmail.com>
2022-06-21 23:15:38 -0600

    g

--- app/Models/Md_customer.php
+++ app/Models/Md_customer.php
@@ -6,12 +6,13 @@ use CodeIgniter\Model;
 
 class Md_customer
 {   
-    protected $table  = 'Tbl_customers';
+    //protected $table  = 'Tbl_customers';
     
     public function getCostumer()
     {
         $db  = \Config\Database::connect();
         //$this->db->table('Tbl_customers');
+        $query = $db->table("Tbl_customers");
         $query = $db->query("call getCustomers()");
         
         return $query->getResult('array');
