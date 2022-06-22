36ae3bea53ee15470b75fa114ff79e5592d6835b
Arsc <mauricio.perez.gomez2020@gmail.com>
2022-06-21 22:20:09 -0600

    fits

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://142.93.115.100:4080/';
 
     /**
      * --------------------------------------------------------------------------
