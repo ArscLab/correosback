40f35231f506bcbc27e4a41841c511b203ecccc1
Arsc <mauricio.perez.gomez2020@gmail.com>
2022-06-21 23:03:17 -0600

    fits

--- app/Models/Md_customer.php
+++ app/Models/Md_customer.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
-$db      = \Config\Database::connect();
-$builder = $db->table('Tbl_customers'); 
-class Md_customer
+
+class Md_customer extends Model
 {
     
     public function getCostumer()
     {
+        //$this->db = \Config\Database::connect();
+        $this->builder = $this->db->table('Tbl_customers'); 
         $query = $this->db->query("call getCustomers()");
         
         return $query->getResult('array');
