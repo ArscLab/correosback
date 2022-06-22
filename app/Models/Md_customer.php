5a78f877ca823b1842b078c9a11d7cf25c8dcf32
Arsc <mauricio.perez.gomez2020@gmail.com>
2022-06-21 23:07:06 -0600

    g

--- app/Models/Md_customer.php
+++ app/Models/Md_customer.php
@@ -5,15 +5,17 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class Md_customer extends Model
-{
-    
+{   
+
     public function getCostumer()
     {
-        $this->db = \Config\Database::connect();
-        $this->builder = $this->db->table('Tbl_customers'); 
-        $query = $this->db->query("call getCustomers()");
+        //$query = $this->db->query("call getCustomers()");
         
-        return $query->getResult('array');
+        //return $query->getResult('array');
+
+
+        $builder = $this->db->table('Tbl_customers');
+        $query   = $builder->get();
     }
 }
 
